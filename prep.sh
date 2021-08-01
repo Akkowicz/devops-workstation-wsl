00a2bab14142f5d681e0844c7a2259d0f94f409d
Mateusz Przybyłowicz <mateusz.przybylowicz@protonmail.com>
2021-08-01 19:30:52 +0200

    Add setup ansible playbook and prep script

---
+++ prep.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install -y python3 python3-pip python-is-python3
+pip install ansible ansible-lint
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.local/bin"' > /etc/environment
+source /etc/environment
+echo "Restart your shell and run the following command: ansible-playbook setup.yml"
