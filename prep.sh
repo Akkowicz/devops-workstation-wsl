9e87cb8d41d1b6c2107e070af813d8c334ebed6b
Mateusz Przybyłowicz <mateusz.przybylowicz@protonmail.com>
2021-09-11 08:41:27 +0200

    Fix editing env file

--- prep.sh
+++ prep.sh
@@ -3,6 +3,6 @@
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install -y python3 python3-pip python-is-python3
 pip install ansible ansible-lint
-echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.local/bin"' > /etc/environment
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.local/bin"' | sudo tee /etc/environment
 source /etc/environment
 echo "Restart your shell and run the following command: ansible-playbook setup.yml"
